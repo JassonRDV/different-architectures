5895642c7be38e880fa06730b032fb4381449b8e
Jasson Ramos <jassonramosdovale@gmail.com>
2025-05-09 21:40:23 -0300

    refactor: Remove unused EntityHabit and update dependencies
    This commit removes the unused `EntityHabit` data class from the database module as it is not currently needed. It also updates several dependencies to their latest versions as specified in the version catalog.
    
    Changes include:
    - Deleted the `EntityHabit.kt` file.
    - Added a new string resource `criar` in `strings.xml` and used it in `create_habit.xml`.
    - Updated `lifecycleRuntimeKtx` and `navigationFragmentKtx` dependencies in `libs.versions.toml` to version 2.9.0.
    - Removed the `material3` dependency from `libs.versions.toml`.

--- core/database/src/main/java/com/example/differentarchitectures/database/EntityHabit.kt
+++ core/database/src/main/java/com/example/differentarchitectures/database/EntityHabit.kt
@@ -1,12 +1,2 @@
 package com.example.differentarchitectures.database
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "habit_table")
-data class EntityHabit(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "habit_name") val habitName: String,
-    @ColumnInfo(name = "habit_local") val habitLocal: String
-)
